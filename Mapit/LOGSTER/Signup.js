f50a65374e8d817ca39bbf67ced9ff5d92295639
Samuelgates4 <130833451+Samuelgates4@users.noreply.github.com>
2023-04-15 15:51:01 +0530

    Add files via upload

---
+++ Mapit/LOGSTER/Signup.js
@@ -0,0 +1,7 @@
+function validateForm()
+{
+  var username = document.getElementById("username").value;
+  var email = document.getElementById("email").value;
+  var password = document.getElementById("password").value;
+
+}
